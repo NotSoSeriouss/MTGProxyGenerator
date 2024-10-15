e4b839981c38211f40c0ac2e5f765de46a3304e9
NotSoSeriouss <superplayer292@gmail.com>
2024-10-15 17:40:17 +0200

    Loading bar

--- js/main_script.js
+++ js/main_script.js
@@ -3,7 +3,7 @@ function request(url) {
 	console.log(url);
 	return new Promise(function (resolve, reject) {
 		const xhr = new XMLHttpRequest();
-		xhr.timeout = 2000;
+		xhr.timeout = 5000;
 		xhr.onreadystatechange = function(e) {
 			if (xhr.readyState === 4) {
 				if (xhr.status === 200) {
@@ -89,20 +89,22 @@ function addImageToDoc(doc){
 	};
 }
 
+var totalImages = 0;
+var imagesDownloaded = 0;
+
+function updateProgressBar() {
+	const progressElement = document.querySelector('.progress');
+	const progressPercentage = (imagesDownloaded / totalImages) * 100;
+	progressElement.style.width = progressPercentage + '%';
+
+	if (progressPercentage > 0) {
+		progressElement.querySelector('.percent').textContent = Math.round(progressPercentage) + '%';
+	}
+}
+
 function getImageUrl(cardNameOrId) {
 	return () => {
-		return request('https://api.scryfall.com/cards/named?exact=' + encodeURIComponent(cardNameOrId))
-			.catch((function(name) {
-				return (error) => request('https://api.scryfall.com/cards/' + encodeURIComponent(name));
-			})(cardNameOrId))
-			.catch(function(name) {
-				return (error) => {
-					while (name.length < 8) {
-						name = '0' + name;
-					}
-					return request('https://api.scryfall.com/cards/' + encodeURIComponent(name));
-				};
-			}(cardNameOrId))
+		return request('https://api.scryfall.com/cards/named?fuzzy=' + encodeURIComponent(cardNameOrId))
 			.then(function(result) {
 				var data = JSON.parse(result);
 				console.log('Requesting result');
@@ -116,6 +118,10 @@ function getImageUrl(cardNameOrId) {
 				} else {
 					throw new Error("Image URL not found");
 				}
+			}).then(function(image) {
+				imagesDownloaded++;
+				updateProgressBar();
+				return image;
 			});
 	};
 }
@@ -123,6 +129,7 @@ function getImageUrl(cardNameOrId) {
 function generateProxies(){
 	imagePos = 0;
 	failedLines = [];
+	imagesDownloaded = 0;
 
 	// create a document the same way as above
 	const doc = new PDFDocument({size: document.getElementById("paper_size").value});
@@ -131,13 +138,14 @@ function generateProxies(){
 	const stream = doc.pipe(blobStream());
 	stream.on('finish', function() {
 		/*var iframe = document.querySelector('iframe');*/
-		// get a blob you can do whatever you like with
+			// get a blob you can do whatever you like with
 		const blob = stream.toBlob('application/pdf');
 		saveAs(blob, "download.pdf");	 
 	});
 
 
 	var lines = document.getElementById("decklist_input").value.split('\n');
+	totalImages = lines.length;
 	var overallProcess = Promise.resolve();
 
 	for(var i = 0; i < lines.length; i++){
@@ -147,32 +155,32 @@ function generateProxies(){
 		}
 
 
-	//var regex_id_nr = 
-	var regex_name = /^(?:([1-9][0-9]*)(?: ))?(.+)/;
-	var regex_result = regex_name.exec(lines[i]);
-	if(regex_result){
-		var number = regex_result[1] === undefined ? 1 : parseInt(regex_result[1]);
-		console.log(lines[i]);
-		console.log(regex_result);
-		console.log("number: " + number);
-		overallProcess = overallProcess.then(getImageUrl(regex_result[2]))
-			.then(
-				function(innerNumber){return (img)=>Promise.all([...Array(innerNumber).keys()].map(i => addImageToDoc(doc)(img)));}(number),
-				function(line){return () => failedLines.push(line);} (regex_result[2])
-			);
+		//var regex_id_nr = 
+			var regex_name = /^(?:([1-9][0-9]*)(?: ))?(.+)/;
+		var regex_result = regex_name.exec(lines[i]);
+		if(regex_result){
+			var number = regex_result[1] === undefined ? 1 : parseInt(regex_result[1]);
+			console.log(lines[i]);
+			console.log(regex_result);
+			console.log("number: " + number);
+			overallProcess = overallProcess.then(getImageUrl(regex_result[2]))
+				.then(
+					function(innerNumber){return (img)=>Promise.all([...Array(innerNumber).keys()].map(i => addImageToDoc(doc)(img)));}(number),
+					function(line){return () => failedLines.push(line);} (regex_result[2])
+				);
 
+		}
 	}
-}
 
-overallProcess = overallProcess
-	.then(()=>{
-		if(failedLines.length != 0){
-			var error_message = "could not process following lines: \n";
-			failedLines.forEach(line => error_message = error_message + "\n" + line);
-			alert(error_message);
-		}})
-	.then(()=>doc.end())
-	.catch(console.log.bind(console));
+	overallProcess = overallProcess
+		.then(()=>{
+			if(failedLines.length != 0){
+				var error_message = "could not process following lines: \n";
+				failedLines.forEach(line => error_message = error_message + "\n" + line);
+				alert(error_message);
+			}})
+		.then(()=>doc.end())
+		.catch(console.log.bind(console));
 }
 
 function dragOverHandler(e) {
